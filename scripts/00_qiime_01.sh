96c2a1e9e872990b4c22395352048f9731d726b5
ljchueca <luisjavier.chueca@ehu.eus>
2024-11-19 16:41:42 +0100

    updating script LJ

--- scripts/00_qiime_01.sh
+++ scripts/00_qiime_01.sh
@@ -12,25 +12,26 @@
 #SBATCH --time=10:00:00
 #SBATCH --mem=24000
 
-
 module load Python/Python-3.10.9-Anaconda3-2023.03-1
 module load Mamba/23.1.0-4
 
-# Qiime 2
-# To activate this environment, use
-#
+# activate Qiime 2 environment
 
 conda activate qiime2-amplicon-2024.10
-
 
 #Input Directory
 IN=/scratch/lchueca/gorbeea_genomic/microbiome_data/2023_DNA/qiime_ITS/data/raw
 #Output Directory
-OUT=demux_paired_end.qza
+OUT=demux_paired_end
 
 
 qiime tools import \
   --type 'SampleData[PairedEndSequencesWithQuality]' \
   --input-path ${IN}/manifest_file.txt \
   --input-format PairedEndFastqManifestPhred33V2 \
-  --output-path ${IN}/${OUT}
+  --output-path ${IN}/${OUT}.qza
+
+
+  qiime demux summarize \
+   --i-data ${IN}/${OUT}.qza \
+   --o-visualization ${IN}/${OUT}.qzv
